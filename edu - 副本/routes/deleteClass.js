35a5f0e54888aa535f775db24c3a1bb56c35f777
1144374507 <1144374507@qq.com>
2021-05-01 11:36:21 +0800

    删除多余副本

--- edu - 副本/routes/deleteClass.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var deleteClass = require('../controllers/deleteClass')
-
-router.delete('/:id', deleteClass.deleteClass);
-
-
-module.exports = router;
--- edu - 副本/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var cate =require('../controllers/cateController')
-/* GET home page. */
-router.get('/', cate.getCate);
-
-module.exports = router;
